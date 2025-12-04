b34cc6f98f5ac1b719e4feae07c186e0398f1c2a
Luiz-RX <luisip2001@gmail.com>
2025-12-04 22:31:17 +0100

    Actividad 2

---
+++ Assets/Scripts/ParticleSystemTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ParticleSystemTrigger : MonoBehaviour
+{
+    public Material targetMaterial;
+    public string floatProperty = ("_MyFloat");
+
+    [Range(-1f, 1f)]
+    public float value = 1f;
+
+    [Header("Particle Settings")]
+    public ParticleSystem particleSystem;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetMaterial.SetFloat(floatProperty, value);
+
+        if (value >= 1 || value <= -1)
+        {
+            particleSystem.Stop();
+        }
+        else
+        {
+            particleSystem.Play();
+        }
+    }
+}
